4ddf295af72cf1596e2272d50c99fa7d197bdee8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-17 08:04:46 -0800

    add contractoraccountingsystem

---
+++ src/ir/university/accounting/app/MainApp.java
@@ -0,0 +1,10 @@
+package ir.university.accounting.app;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
